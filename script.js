4dfa4795d4e3680370677ce8469a3710ca4ca53f
aki395 <154888741+aki395@users.noreply.github.com>
2024-08-31 23:21:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,18 +16,18 @@ function showQuiz(){
  }
  let questions = [
     {
-        img:"images/plate1.jpg",
+        img:"plate1.jpg",
         options : [12,72,"Nothing"],
         answer : 12
     },
     {
-        img:"images/plate2.jpg",
+        img:"plate2.jpg",
         options : [3,0,8],
         answer : 8,
         Red_green_deficiency: 3
     },
     {
-        img:"images/plate3.jpg",
+        img:"plate3.jpg",
         options : [5,6,0],
         answer : 6,
         Red_green_deficiency: 5 
@@ -35,152 +35,152 @@ function showQuiz(){
 
     },
     {
-        img:"images/plate4.jpg",
+        img:"plate4.jpg",
         options : [20,29,70],
         answer : 29,
         Red_green_deficiency: 70 
     },
     {
-        img:"images/plate5.jpg",
+        img:"plate5.jpg",
         options : [35,51,57],
         answer : 57,
         Red_green_deficiency: 35 
     },
     {
-        img:"images/plate6.jpg",
+        img:"plate6.jpg",
         options : [5,2,6],
         answer : 5,
         Red_green_deficiency: 2
     },
     {
-        img:"images/plate7.jpg",
+        img:"plate7.jpg",
         options : [6,5,3],
         answer : 3,
         Red_green_deficiency: 5 
     },
     {
-        img:"images/plate8.jpg",
+        img:"plate8.jpg",
         options : [15,17,57],
         answer : 15,
         Red_green_deficiency: 17 
     },
     {
-        img:"images/plate9.jpg",
+        img:"plate9.jpg",
         options : [57,74,21],
         answer : 74,
         Red_green_deficiency: 21 
 
     },
     {
-        img:"images/plate10.jpg",
+        img:"plate10.jpg",
         options : [2,5,"Nothing"],
         answer : 2,
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate11.jpg",
+        img:"plate11.jpg",
         options : ["Nothing",6,5],
         answer : 6,
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate12.jpg",
+        img:"plate12.jpg",
         options : [57,"Nothing",97],
         answer : 97,
         Red_green_deficiency:"Nothing" 
     },
     {
-        img:"images/plate13.jpg",
+        img:"plate13.jpg",
         options : ["Nothing",45,65],
         answer : 45,
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate14.jpg",
+        img:"plate14.jpg",
         options : [5,8,"Nothing"],
         answer : 5,
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate15.jpg",
+        img:"plate15.jpg",
         options : ["Nothing",4,7],
         answer : 7,
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate16.jpg",
+        img:"plate16.jpg",
         options : [76,16,"Nothing"],
         answer : 16,
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate17.jpg",
+        img:"plate17.jpg",
         options : [73,"Nothing",28],
         answer : 73,
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate18.jpg",
+        img:"plate18.jpg",
         options : ["Nothing",5,6],
         answer : "Nothing",
         Red_green_deficiency: 5
     },
     {
-        img:"images/plate19.jpg",
+        img:"plate19.jpg",
         options : [7,2,"Nothing"],
         answer : "Nothing",
         Red_green_deficiency: 2 
     },
     {
-        img:"images/plate20.jpg",
+        img:"plate20.jpg",
         options : ["Nothing",45,18],
         answer : "Nothing",
         Red_green_deficiency: 45
     },
     {
-        img:"images/plate21.jpg",
+        img:"plate21.jpg",
         options : [73,"Nothing",18],
         answer : "Nothing",
         Red_green_deficiency: 73 
 
     },
     {
-        img:"images/plate22.jpg",
+        img:"plate22.jpg",
         options : [2,26,6],
         answer : 26,
         Protanopia: 6 ,
         Deuteranopia: 2
     },
     {
-        img:"images/plate23.jpg",
+        img:"plate23.jpg",
         options : [2,4,42],
         answer : 42,
         Protanopia : 2, 
         Deuteranopia : 4 
     },
     {
-        img:"images/plate24.jpg",
+        img:"plate24.jpg",
         options : [5,35,3],
         answer : 35,
         Protanopia : 5, 
         Deuteranopia : 3
     },
     {
-        img:"images/plate25.jpg",
+        img:"plate25.jpg",
         options : [96,9,6],
         answer : 96,
         Protanopia : 6, 
         Deuteranopia : 9
     },
     {
-        img:"images/plate26.jpg",
+        img:"plate26.jpg",
         options : ["Purple line","Purple and red line","Red line"],
         answer : "Purple and red line",
         Protanopia : "Purple line", 
         Deuteranopia : "Red line"
     },
     {
-        img:"images/plate27.jpg",
+        img:"plate27.jpg",
         options : ["Red line","Purple line","Purple and red line"],
         answer : "Purple and red line",
         Protanopia :"Purple line", 
@@ -188,68 +188,68 @@ function showQuiz(){
         
     },
     {
-        img:"images/plate28.jpg",
+        img:"plate28.jpg",
         options : ["Nothing","Line",0],
         answer : "Nothing",
         Red_green_deficiency: "Line" 
     },
     {
-        img:"images/plate29.jpg",
+        img:"plate29.jpg",
         options : ["Line",19,"Nothing"],
         answer : "Nothing",
         Red_green_deficiency: "Line"
     },
     {
-        img:"images/plate30.jpg",
+        img:"plate30.jpg",
         options : ["Red-green line","Nothing","Blue-green line"],
         answer : "Blue-green line",
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate31.jpg",
+        img:"plate31.jpg",
         options : ["Blue-green line","Red-green line","Nothing"],
         answer : "Blue-green line",
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate32.jpg",
+        img:"plate32.jpg",
         options : ["Nothing","Orange line","Red line"],
         answer : "Orange line",
         Red_green_deficiency: "Nothing"
     },
     {
-        img:"images/plate33.jpg",
+        img:"plate33.jpg",
         options : ["Orange line","Red line","Nothing"],
         answer : "Orange line",
         Red_green_deficiency: "Nothing" 
     },
     {
-        img:"images/plate34.jpg",
+        img:"plate34.jpg",
         options : ["Red-green and violet line","Violet line","Blue-green and yellow-green line"],
         answer : "Blue-green and yellow-green line",
         Red_green_deficiency: " Red-green and violet line"
     },
     {
-        img:"images/plate35.jpg",
+        img:"plate35.jpg",
         options : ["Blue-green and yellow-green line","Blue-green and Violet line","Blue line"],
         answer : "Blue-green and yellow-green line",
         Red_green_deficiency: "Blue-green and violet line"
     },
     {
-        img:"images/plate36.jpg",
+        img:"plate36.jpg",
         options : ["Violet and orange line","Blue-green and violet line","Violet line"],
         answer : "Violet and orange line",
         Red_green_deficiency: "Blue-green and violet line" 
 
     },
     {
-        img:"images/plate37.jpg",
+        img:"plate37.jpg",
         options : ["Violet line","Violet and orange line","Blue-green and violet line"],
         answer : "Violet and orange line",
         Red_green_deficiency: "Blue-green and violet line"
     },
     {
-        img:"images/plate38.jpg",
+        img:"plate38.jpg",
         options : ["Line","Nothing","Circle"],
         answer : "Line",
     }
